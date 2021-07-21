a1a1487536ef494eecbd32f1fb024accfd9052e9
pranaysurve <pranaysurve75@gmail.com>
2021-07-21 13:10:40 +0530

    created form and ajax request

---
+++ assets/data.php
@@ -0,0 +1,8 @@
+<?php 
+$post_data = $_POST; 
+$fname = $post_data['firstname'];
+$lname = $post_data['lastname'];
+$uemail = $post_data['email'];
+$contno = $post_data['contno'];
+
+?>
--- assets/navmenu.js
+++ assets/navmenu.js
@@ -1,5 +1,35 @@
+$('document').ready(function(){
+
 $('.navbar-light .dmenu').hover(function () {
   $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
 }, function () {
   $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+});
+function resetForm(formid) {
+  $(':input','#'+formid) .not(':button, :submit, :reset, :hidden') .val('')
+    .removeAttr('checked') .removeAttr('selected');
+   }
+   
+jQuery("form").submit(function (event){
+  // console.log( $( this ).serializeArray() );
+  event.preventDefault();
+  var myObject = [];
+  myObject = jQuery('#contactus').serializeArray();
+  // console.log(myObject);
+  
+  $.ajax({
+    type:'POST',
+    url:'assets/data.php',
+    data:myObject,
+    // dataType:'json',
+    success:function(tst){
+      console.log(tst);
+      alert('test');
+    },
+    complete: function (){
+      resetForm('contactus');
+    }
+  });
+});
+
 });
